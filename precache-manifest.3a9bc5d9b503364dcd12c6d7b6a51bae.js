f877fd492ce8e9ce6dd11227727efad477e7c82b
saransh619 <saranshpachhai@gmail.com>
2020-10-23 08:04:38 +0545

    Updates

---
+++ precache-manifest.3a9bc5d9b503364dcd12c6d7b6a51bae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340d0ac2ff7e43c7a73602eda7636585",
+    "url": "/reactanimatedwebsite/index.html"
+  },
+  {
+    "revision": "6b4ede1899b4c350c1c1",
+    "url": "/reactanimatedwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "8529579282ec9a5f58ae",
+    "url": "/reactanimatedwebsite/static/css/main.b72a43a0.chunk.css"
+  },
+  {
+    "revision": "6b4ede1899b4c350c1c1",
+    "url": "/reactanimatedwebsite/static/js/2.7d2f465a.chunk.js"
+  },
+  {
+    "revision": "e53cf286e00aab3498ae2b2d3185567a",
+    "url": "/reactanimatedwebsite/static/js/2.7d2f465a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8529579282ec9a5f58ae",
+    "url": "/reactanimatedwebsite/static/js/main.f2e4ecb5.chunk.js"
+  },
+  {
+    "revision": "802de9f36ab20c583325",
+    "url": "/reactanimatedwebsite/static/js/runtime-main.687b0aa0.js"
+  },
+  {
+    "revision": "5b67f91f24bfdcfc9453ee57ee4af393",
+    "url": "/reactanimatedwebsite/static/media/AndroidStudio-Intro-feature.5b67f91f.png"
+  },
+  {
+    "revision": "86dcc26e54aeb4da1001a2b9a32839bb",
+    "url": "/reactanimatedwebsite/static/media/Custom-Software-Development.86dcc26e.jpg"
+  },
+  {
+    "revision": "c5a2d6e272d729f2706a9d09b864aad3",
+    "url": "/reactanimatedwebsite/static/media/digital-marketing.c5a2d6e2.png"
+  },
+  {
+    "revision": "f3668a1df5e7c0e7270d9b18d44f0870",
+    "url": "/reactanimatedwebsite/static/media/home.f3668a1d.svg"
+  },
+  {
+    "revision": "c0a3c656af08ed90fb4fb45481075bcd",
+    "url": "/reactanimatedwebsite/static/media/mad_blog_5df2190b8638a1576147211.c0a3c656.jpg"
+  },
+  {
+    "revision": "52760becb07b61214e9e7eb0036a497f",
+    "url": "/reactanimatedwebsite/static/media/marketing-plan_1.52760bec.jpg"
+  },
+  {
+    "revision": "1a567371032ffb1b69c5d719f79f5899",
+    "url": "/reactanimatedwebsite/static/media/technical-support-137-1087325.1a567371.png"
+  },
+  {
+    "revision": "089c6f85929620dfab10728afb9fe828",
+    "url": "/reactanimatedwebsite/static/media/web.089c6f85.png"
+  }
+]);
